3bb2c0dc6ea55809826c4f7f7ba8290ee5e500c9
Iflal-Ahamed <iflaliflal401@gmail.com>
2026-07-29 19:51:07 +0530

    Config Files updated

---
+++ web/mail/mail_config.local.example.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Local SMTP overrides (DO NOT COMMIT).
+ *
+ * Gmail setup:
+ * - MAIL_USERNAME: your Gmail address
+ * - MAIL_PASSWORD: Google App Password (16 chars)
+ * - encryption: tls, port 587
+ */
+return [
+    "host" => "smtp.gmail.com",
+    "port" => 587,
+    "username" => "iflaliflal401@gmail.com",
+    "password" => "PUT_YOUR_GMAIL_APP_PASSWORD_HERE",
+    "from_email" => "iflaliflal401@gmail.com",
+    "from_name" => "R-TEL Mobile Shop",
+    "encryption" => "tls",
+];
